d3db237217b6e88b8da39e8a36bc19038b46139e
JW-sudo <janetwang20161@gmail.com>
2019-10-07 23:06:54 -0700

    Updates

---
+++ precache-manifest.1849a95e3cf91f5f789ab8a2759c5fdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27459fc48ed58ed8326555d791a5883",
+    "url": "/my_design/index.html"
+  },
+  {
+    "revision": "df00706a5af7701b3e98",
+    "url": "/my_design/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "97f96d74735f7754de57",
+    "url": "/my_design/static/css/main.e2deda98.chunk.css"
+  },
+  {
+    "revision": "df00706a5af7701b3e98",
+    "url": "/my_design/static/js/2.74aea4ca.chunk.js"
+  },
+  {
+    "revision": "97f96d74735f7754de57",
+    "url": "/my_design/static/js/main.5acf696f.chunk.js"
+  },
+  {
+    "revision": "c9d2a9c042604ffb7bae",
+    "url": "/my_design/static/js/runtime-main.c2cd853d.js"
+  },
+  {
+    "revision": "4e75dffc8dcd0678f21f0ad281dd2193",
+    "url": "/my_design/static/media/header_icon.4e75dffc.png"
+  },
+  {
+    "revision": "f355c337b6313b09ddb14b966edf7e74",
+    "url": "/my_design/static/media/uni.f355c337.jpg"
+  }
+]);
